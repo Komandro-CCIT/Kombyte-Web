2fceeb0731bc6bac0de41970d710c56cca2c899d
Ifarra <ifarra1324@gmail.com>
2024-02-10 00:53:21 +0700

    add start

--- js/page/comp.js
+++ js/page/comp.js
@@ -18,12 +18,15 @@ $fileInput.on('change', function () {
     let fileName = $(this).val().split('\\').pop();
     $textContainer.text(fileName);
     $('.item-delete').css('display', 'inline-block');
+    $('.submit').css('display', 'flex');
   } else if (filesCount === 0) {
     $textContainer.text('or drop files here');
     $('.item-delete').css('display', 'none');
+    $('.submit').css('display', 'none');
   } else {
     $textContainer.text(filesCount + ' files selected');
     $('.item-delete').css('display', 'inline-block');
+    $('.submit').css('display', 'flex');
   }
 });
 
@@ -31,4 +34,33 @@ $delete.on('click', function () {
   $('.file-input').val(null);
   $('.file-msg').text('or drop files here');
   $('.item-delete').css('display', 'none');
+  $('.submit').css('display', 'none');
 });
+
+// Wait for the page to load
+document.addEventListener("DOMContentLoaded", function() {
+
+  // Get the elements with the respective IDs
+  var submisiContainer = document.getElementById("submisi");
+  var startContainer = document.getElementById("start");
+  var timerContainer = document.getElementById("timerduh");
+
+  // Hide the submisi container initially
+  submisiContainer.classList.add("hide");
+  timerContainer.classList.add("hide");
+
+  // Show the start container initially
+  startContainer.classList.remove("hide");
+
+  // Add click event listener to the start button
+  var startButton = document.querySelector("#start button");
+  startButton.addEventListener("click", function() {
+    // Hide the start container
+    startContainer.classList.add("hide");
+
+    // Show the submisi container
+    submisiContainer.classList.remove("hide");
+    timerContainer.classList.remove("hide");
+  });
+});
+
